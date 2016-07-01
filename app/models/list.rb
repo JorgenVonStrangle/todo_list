ddb2a4610ea4087fbf5f1bfbeb0cf9f679628b79
Jorge Alejandro Rojas Gómez <jorge.rojas@softtek.com>
2016-06-30 19:51:47 -0500

    Se agregaron las validaciones a Task y List con validates :title precence: true

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,3 @@
 class List < ActiveRecord::Base
+		validates :title precence: true
 end
--- db/migrate/20160701004316_create_tasks.rb
+++ db/migrate/20160701004316_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string, :title
-      t.text, :note
+      t.string :title
+      t.text :note
       t.boolean :completed
 
       t.timestamps null: false
