a36738d495dfb6275fdc70552d061bd1a04916d5
Jorge Alejandro Rojas Gómez <jorge.rojas@softtek.com>
2016-06-30 21:54:04 -0500

    Se ha añadido Todo, y funciona como se esperaba

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	validates :name, :lastname, :username, :email precence: true
-	validates :username, :email uniqueness: true
+	validates :name, :lastname, :username, :email, presence: true
+	validates :username, :email, uniqueness: true
 	has_many :lists, dependent: :destroy
 end
