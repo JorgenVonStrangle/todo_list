f613faf6ead2de9d2543a120bca59c9dc230dd56
Jorge Alejandro Rojas Gómez <jorge.rojas@softtek.com>
2016-06-30 20:05:34 -0500

    Se crearon los dos SCOPE, uno para obtener las tareas terminadas y otro para las tareas incompletas. 1. scope :task_end, -> { where(completed: true) } 2. scope :task_end, -> { where(completed: false) }

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,6 @@ class Task < ActiveRecord::Base
 	validates :title precence: true
 
 	belongs_to :list
+	scope :task_end, -> { where(completed: true) }
+	scope :task_end, -> { where(completed: false) }
 end
